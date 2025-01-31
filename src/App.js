f19b75a380c1486db7fbb22235495f282f88d936
Anil Sahu <anil.sahu@genzeon.com>
2025-01-31 17:20:41 +0530

    text changed

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, Welcome to Integration Ninjas
+          Hello, Welcome to Github pages
         </p>
         <a
           className="App-link"
